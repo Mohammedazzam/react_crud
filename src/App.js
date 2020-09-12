7db8651454f37a9c654d440c89af9015be658f0c
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-12 23:03:10 +0300

    (Create App && All Components)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CoursForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+  
+  state ={
+    courses:[
+      {name: "HTML"},
+      {name: "CSS"},
+      {name: "JQuery"},
+    ]
+  }
+
+  render() {
+    return (
+      <section className="App">
+        <h2>Add Course</h2>
+        <CoursForm />
+        <CourseList />
+      </section>
+    );
+  }
 }
-
 export default App;
